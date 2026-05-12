7003b0279f8a7ebeb27565a19a69b74e46fda749
yyyhelloworld2 <2914279946@qq.com>
2026-05-12 20:43:32 +0800

    Add files via upload

---
+++ js/right.js
@@ -0,0 +1,23 @@
+function click(e) {
+if (document.all) {
+if (event.button==2||event.button==3) {
+oncontextmenu='return false';
+}
+}
+if (document.layers) {
+if (e.which == 3) {
+oncontextmenu='return false';
+}
+}
+}
+if (document.layers) {
+document.captureEvents(Event.MOUSEDOWN);
+}
+document.onmousedown=click;
+document.oncontextmenu = new Function("return false;")
+document.onkeydown =document.onkeyup = document.onkeypress=function(){ 
+if(window.event.keyCode == 123) { 
+window.event.returnValue=false;
+return(false); 
+} 
+}
